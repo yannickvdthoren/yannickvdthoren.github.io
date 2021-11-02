40e021a5a53f06bf7b3a62f8ddb5facd594eb8d6
Yannick Vandenthoren <yannickvdthoren@gmail.com>
2021-11-02 09:43:29 +0100

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -23,7 +23,7 @@
       </ul>
       <p>
         I’m a passionate <strong>Product Designer</strong> and
-        <strong>Full Stack Developper</strong> adept at unlocking the full
+        <strong>Full Stack Developer</strong> adept at unlocking the full
         potential of my clients - often in dire need of a great design, a solid
         UX or even a full redesign. See
         <a href="https://www.linkedin.com/in/yannickvdthoren/"
