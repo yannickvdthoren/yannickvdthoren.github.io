6b856cd763f6fb41624602b50a3ffecf6f817257
Yannick Vandenthoren <yannickvdthoren@gmail.com>
2023-01-03 12:13:49 +0100

    Import list of work

--- pages/index.vue
+++ pages/index.vue
@@ -9,18 +9,18 @@
 
 <script>
 import Header from '@/components/Header.vue'
-// import List from '@/components/List.vue'
+import List from '@/components/List.vue'
 export default {
   name: 'index',
   components: {
     Header
   },
-  // async asyncData({ $content, params }) {
-  //   const works = await $content('work').sortBy('position', 'asc').fetch()
-  //   const side = await $content('side-project')
-  //     .sortBy('position', 'asc')
-  //     .fetch()
-  //   return { works, side }
-  // },
+  async asyncData({ $content, params }) {
+    const works = await $content('work').sortBy('position', 'asc').fetch()
+    const side = await $content('side-project')
+      .sortBy('position', 'asc')
+      .fetch()
+    return { works, side }
+  },
 }
 </script>
