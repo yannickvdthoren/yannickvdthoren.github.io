f008d1e09423cc86e93d6fce1a23c748f5ea67e7
yannickvdthoren <yannickvdthoren@users.noreply.github.com>
2023-05-16 06:22:27 +0000

    deploy: a7b8a7509c6e5a76dc32df2c9cd6ab0c23f37757

---
+++ _nuxt/static/1684218144/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1684218144",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"853b2b5a"}}}}(null,"\u002F"));
